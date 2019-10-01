036ebfabf533786d47d584ac2fc1a5bd9e734a8a
Shengjie-Sun <Shengjie-Sun@users.noreply.github.com>
2019-10-01 16:10:33 -0400

    "dfs with comments"

--- 200-Medium-Number of Islands-[DFS-BFS-UF].py
+++ 200-Medium-Number of Islands-[DFS-BFS-UF].py
@@ -2,6 +2,7 @@ di = [0, 0, -1, 1]
 dj = [1, -1, 0, 0]
 
 class Solution:        
+    # The first corrent version I have written "fast" but dirty.
     def numIslands_dfs_visited_ugly(self, grid):
         if not grid: return 0
         n, m = len(grid), len(grid[0])
@@ -11,12 +12,14 @@ class Solution:
             visited.add((i, j))
             for d in range(len(di)):
                 ni, nj = i + di[d], j + dj[d]
+                # Put the if condition here leads to redundancy.
                 if 0<=ni<n and 0<=nj<m and grid[ni][nj]=="1" and (ni, nj) not in visited:
                     dfs(ni, nj)
                 else:
                     visited.add((ni, nj))
             return 1
         
+        # The judge statement is a must becasue as long as dfs called, it returns 1.
         return sum([dfs(i, j) for i in range(n) for j in range(m) if (i, j) not in visited and grid[i][j]=="1"])
 
     def numIslands_dfs_visited_clean(self, grid):
@@ -25,6 +28,7 @@ class Solution:
         visited = set()
         
         def dfs(i, j):
+            # Put the if condition here is suitable!
             if 0<=i<n and 0<=j<m and grid[i][j]=="1" and (i, j) not in visited:
                 visited.add((i, j))
                 for d in range(len(di)):
@@ -34,6 +38,7 @@ class Solution:
         
         return sum([dfs(i, j) for i in range(n) for j in range(m)])
     
+    # Modified from Stefan Pochmann, changing the grid is not good but "map" make the codes lean.
     def numIslands_dfs_floodfill(self, gird):
         if not grid: return 0
 
@@ -54,5 +59,5 @@ if __name__ == "__main__":
     grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
 
     sol = Solution()
-    print(sol.numIslands_dfs_floodfill(grid))
+    print(sol.numIslands_dfs_visited_ugly(grid))
     
