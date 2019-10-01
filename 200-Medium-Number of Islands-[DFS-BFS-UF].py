9f4303b88e20e3efdb847102bdc4ae16f0bc0410
Shengjie-Sun <Shengjie-Sun@users.noreply.github.com>
2019-10-01 15:39:04 -0400

    'first commit'

---
+++ 200-Medium-Number of Islands-[DFS-BFS-UF].py
@@ -0,0 +1,58 @@
+di = [0, 0, -1, 1]
+dj = [1, -1, 0, 0]
+
+class Solution:        
+    def numIslands_dfs_visited_ugly(self, grid):
+        if not grid: return 0
+        n, m = len(grid), len(grid[0])
+        visited = set()
+        
+        def dfs(i, j):
+            visited.add((i, j))
+            for d in range(len(di)):
+                ni, nj = i + di[d], j + dj[d]
+                if 0<=ni<n and 0<=nj<m and grid[ni][nj]=="1" and (ni, nj) not in visited:
+                    dfs(ni, nj)
+                else:
+                    visited.add((ni, nj))
+            return 1
+        
+        return sum([dfs(i, j) for i in range(n) for j in range(m) if (i, j) not in visited and grid[i][j]=="1"])
+
+    def numIslands_dfs_visited_clean(self, grid):
+        if not grid: return 0
+        n, m = len(grid), len(grid[0])
+        visited = set()
+        
+        def dfs(i, j):
+            if 0<=i<n and 0<=j<m and grid[i][j]=="1" and (i, j) not in visited:
+                visited.add((i, j))
+                for d in range(len(di)):
+                    dfs(i+di[d], j+dj[d])
+                return 1
+            return 0
+        
+        return sum([dfs(i, j) for i in range(n) for j in range(m)])
+    
+    def numIslands_dfs_floodfill(self, gird):
+        if not grid: return 0
+
+        n, m = len(grid), len(grid[0])
+        def dfs(i, j):
+            if 0<=i<n and 0<=j<m and grid[i][j]=="1":
+                grid[i][j]="0"
+                list(map(dfs, (i+1, i-1, i, i), (j, j, j+1, j-1)))
+                return 1
+            return 0
+
+        return sum([dfs(i, j) for i in range(n) for j in range(m)])
+    
+
+        
+if __name__ == "__main__":
+    # Test Case
+    grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+
+    sol = Solution()
+    print(sol.numIslands_dfs_floodfill(grid))
+    
